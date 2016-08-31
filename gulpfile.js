b8e94d848d4de50477b58cbc60bed5335bdea032
Marek Podsiadły <marek.podsiadly@nortpoint.io>
2016-08-31 14:54:09 +0200

    load sample data

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,20 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var sass = require('gulp-sass');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
+    browserSync.init({ server: './src' });
 
-    browserSync.init({
-        server: "./src"
-    });
-
-    gulp.watch("src/scss/*.scss", ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch('src/scss/*.scss', ['sass']);
+    gulp.watch(['src/*.html', 'src/js/*.js']).on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("src/scss/*.scss")
+    return gulp.src('src/scss/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest('src/css'))
         .pipe(browserSync.stream());
 });
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,49 @@
 (function() {
     'use strict';
+
+    var app = {
+        isLoading: true,
+        basket: [],
+        pizzas: [],
+        spinner: document.querySelector('.spinner'),
+        pizzasListContainer: document.querySelector('.pizza-list'),
+        pizzasList: document.querySelector('.pizza-list > ul'),
+        pizzaListItemTemplate: document.querySelector('.pizzaListItemTemplate')
+    };
+
+    app.getPizzas = function() {
+        var url = '/api/pizzas.json';
+
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    var response = JSON.parse(request.response);
+                    app.updatePizzasList(response);
+                }
+            }
+        };
+        request.open('GET', url);
+        request.send();
+    };
+
+    app.updatePizzasList = function(pizzas) {
+        for (var i = 0; i < pizzas.length; i++) {
+            var pizza = pizzas[i];
+            var item = app.pizzaListItemTemplate.cloneNode(true);
+            item.classList.remove('pizzaListItemTemplate');
+            item.querySelector('.pizza-price').textContent = pizza.price
+                                                                    + ' PLN';
+            item.querySelector('.pizza-name').textContent = pizza.name;
+            app.pizzasList.appendChild(item);
+        }
+
+        if (app.isLoading) {
+            app.spinner.setAttribute('hidden', true);
+            app.pizzasListContainer.removeAttribute('hidden');
+            app.isLoading = false;
+        }
+    };
+
+    app.getPizzas();
 })();
