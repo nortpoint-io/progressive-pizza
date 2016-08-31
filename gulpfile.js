15fa7e94b482808b35b68d3aa0c3bc03bcff5b39
Marek Podsiadły <marek.podsiadly@nortpoint.io>
2016-08-31 15:31:07 +0200

    gulp task for generate service-worker.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var packageJson = require('./package.json');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
     browserSync.init({ server: './src' });
 
+    gulp.watch([
+        'src/*.html',
+        'src/js/*.js',
+        'src/img/*.jpg'
+    ], ['generate-service-worker']);
     gulp.watch('src/scss/*.scss', ['sass']);
-    gulp.watch(['src/*.html', 'src/js/*.js']).on('change', browserSync.reload);
+    gulp.watch('src/service-worker.js').on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -18,4 +24,34 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+// create service worker file
+gulp.task('generate-service-worker', function(callback) {
+    var path = require('path');
+    var swPrecache = require('sw-precache');
+    var rootDir = 'src';
+
+    swPrecache.write(path.join(rootDir, 'service-worker.js'), {
+        staticFileGlobs: [rootDir + '/**/*.{js,html,css,jpg}'],
+        stripPrefix: rootDir,
+        cacheId: packageJson.name,
+        runtimeCaching: [{
+            urlPattern: /\/api\//,
+            handler: 'cacheFirst'
+        }, {
+            urlPattern: /img/,
+            handler: 'cacheFirst',
+            options: {
+                cache: {
+                    name: 'image-cache',
+                    // Use sw-toolbox's LRU expiration.
+                    maxEntries: 50
+                }
+            }
+        }, {
+            // Use a network first strategy for everything else.
+            default: 'networkFirst'
+        }]
+    }, callback);
+});
+
+gulp.task('default', ['sass', 'generate-service-worker', 'serve']);
