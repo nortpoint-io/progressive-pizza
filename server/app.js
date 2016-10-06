af1d7e859a25aa08933c1d7f16af743e051db2de
grzegorz.bednarski <grzegorz.bednarski@nortpoint.io>
2016-10-06 11:53:46 +0200

    server app for sending messages to apps.

---
+++ server/app.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+const router = express.Router();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+const FIREBASE_AUTHORIZATION_KEY = 'AIzaSyCb9ysSZxINYbX6QjruMT4wF85Mywvxi2U';
+let SUBSRIPTIONS = {};
+
+
+let indexHandler = function(req, res) {
+    res.render('index', {});
+}
+
+let subscribeHandler = function(req, res) {
+    let endpoint = req.body.subscription.endpoint;
+    SUBSRIPTIONS[endpoint] = {};
+    res.send({ status: 201 });
+}
+
+let sendMessageHandler = function(req, res) {
+    let registrationId = req.body['registration-id'] ;
+    let requestBody = {
+        registration_ids: [registrationId]
+    };
+
+    let request = http.request({
+        protocol: 'http:',
+        host: 'android.googleapis.com',
+        method: 'POST',
+        path: '/gcm/send',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `key=${FIREBASE_AUTHORIZATION_KEY}`
+        },
+
+    }, function(response) {
+        let data = '';
+        let context = {}
+
+        if (response.statusCode == 401) {
+            context.error = true;
+            context.errorMsg = 'Something is wrong with Firebase authorization key.';
+            context.badKey = true;
+            res.render('index', context);
+
+            return;
+        }
+
+        response.on('data', function (chunk) {
+            data += chunk;
+        });
+
+        response.on('end', function () {
+            let body = JSON.parse(data);
+
+            if (body.success) {
+                context.success = true;
+                context.successMsg = `Successfully send message to ${registrationId}`;
+
+            } else {
+                context.error = true;
+                context.responseError = true;
+
+                if (body.results[0].error === 'InvalidRegistration') {
+                    context.errorMsg = `This ${registrationId} is invalid app registration id.`
+                } else {
+                    context.errorMsg = body.results[0].error;
+                }
+            }
+            res.render('index', context);
+        });
+    });
+
+    request.write(JSON.stringify(requestBody));
+    request.end();
+}
+
+router.get('/', indexHandler);
+router.post('/', sendMessageHandler);
+router.post('/subscribe', subscribeHandler);
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3010, function() {
+    console.log('app started');
+});
